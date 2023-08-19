282c438f5cffa608bfdae0c3e2cb66e5aa4e6bbb
Rishav-27 <111058208+Rishav-27@users.noreply.github.com>
2023-08-19 14:30:19 +0530

    Update errorandfunctions.sol

--- errorandfunctions.sol
+++ errorandfunctions.sol
@@ -6,9 +6,9 @@ contract SolidityErrorHandles {
     // uint public num = 0;
     uint256 public billing_amt;
 
-    function minting(uint _unitreached) public {
-        require(unitreaches > 300, "Your Electricity bill is free.");
-        billing_amt = (unintreaches-300)*10;
+    function minting(uint reading) public {
+        require(reading > 300, "Your Electricity bill is free.");
+        billing_amt = (reading-300)*10;
     }
     function voter_age_check(uint age) public pure returns (uint){
         if(age<18){
@@ -21,5 +21,5 @@ contract SolidityErrorHandles {
         function no_of_year_of_punishment(uint _crimes) public pure returns(uint256){
         assert(_crimes>5);// you will be hanged to death,No mercy..
         return _crimes*4;
-    }
+        }
 }
